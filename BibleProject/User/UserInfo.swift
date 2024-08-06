0610a7a4e7c98c19a9acbed5999f2c175135b73c
kwakhyeonwoo <polla723@naver.com>
2024-08-06 20:40:46 +0900

    TextFieldLayout

--- BibleProject/User/UserInfo.swift
+++ BibleProject/User/UserInfo.swift
@@ -17,109 +17,108 @@ struct UserInfo: View {
     @State var weight: String = ""
     @State var age: String = ""
     
-    //Alert 표시 여부 제어하는 변수
+    // Alert 표시 여부 제어하는 변수
     @State private var showAlert = false
     @State private var alertMessage = ""
     
     var body: some View {
-        VStack {
+        VStack (alignment:.center){
             Image("Paris5")
-            .resizable()
-            .frame(height: 150)
-            .clipShape(Circle())
+                .resizable()
+                .frame(height: 150)
+                .clipShape(Circle())
+                .padding(.bottom, 50)
             
-            HStack(alignment: .center) {
-                Text("닉네임: ")
-                    .padding()
-                TextField("닉네임", text: $nickName)
-                    .padding()
-                    .textInputAutocapitalization(.never)
-                    .disableAutocorrection(true)
-                    .onAppear() {
-                        UITextField.appearance().clearButtonMode = .whileEditing
-                    }
-            }
-            .padding(.horizontal)
-            
-            HStack {
-                Text("성별: ")
-                    .padding()
+            Group {
+                HStack {
+                    Text("닉네임: ")
+                        .frame(width: 80, alignment: .leading)
+                    TextField("닉네임", text: $nickName)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.trailing)
+                        .textInputAutocapitalization(.never)
+                        .disableAutocorrection(true)
+                        .onAppear() {
+                            UITextField.appearance().clearButtonMode = .whileEditing
+                        }
+                }
                 
-                Button(action: {
-                    self.gender = true
-                }, label: {
-                    Text("남성")
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(self.gender == true ? Color.blue : Color.gray)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                })
+                HStack {
+                    Text("성별: ")
+                        .frame(width: 80, alignment: .leading)
+                    Button(action: {
+                        self.gender = true
+                    }, label: {
+                        Text("남성")
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(self.gender == true ? Color.blue : Color.gray)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    })
+                    
+                    Button(action: {
+                        self.gender = false
+                    }, label: {
+                        Text("여성")
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(self.gender == false ? Color.pink : Color.gray)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    })
+                }
                 
-                Button(action: {
-                    self.gender = false
-                }, label: {
-                    Text("여성")
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(self.gender == false ? Color.pink : Color.gray)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                })
-            }
-            .padding(.horizontal)
-            
-            HStack {
-                Text("키: ")
-                    .padding()
-                TextField("키", text: $height)
-                    .keyboardType(.decimalPad)
-                    .padding()
-                    .onChange(of: height) {heightValue in
-                        if !heightValue.allSatisfy({$0.isNumber || $0 == "."}){
-                            showAlert = true
-                            alertMessage = "키만 입력하세요!"
-                            height = heightValue.filter{$0.isNumber || $0 == "."}
+                HStack {
+                    Text("키: ")
+                        .frame(width: 80, alignment: .leading)
+                    TextField("키", text: $height)
+                        .keyboardType(.decimalPad)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .onChange(of: height) { heightValue in
+                            if !heightValue.allSatisfy({ $0.isNumber || $0 == "." }) {
+                                showAlert = true
+                                alertMessage = "숫자만 입력하세요!"
+                                height = heightValue.filter { $0.isNumber || $0 == "." }
+                            }
                         }
-                    }
-            }
-            .padding(.horizontal)
-            
-            HStack {
-                Text("몸무게: ")
-                    .padding()
-                TextField("몸무게", text: $weight)
-                    .keyboardType(.decimalPad)
-                    .padding()
-                    .onChange(of: weight){ weightValue in
-                        if !weightValue.allSatisfy({$0.isNumber || $0 == "."}){
-                            showAlert = true
-                            alertMessage = "몸무게만 입력하세요!"
-                            weight = weightValue.filter{$0.isNumber || $0 == "."}
+                }
+                
+                HStack {
+                    Text("몸무게: ")
+                        .frame(width: 80, alignment: .leading)
+                    TextField("몸무게", text: $weight)
+                        .keyboardType(.decimalPad)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .onChange(of: weight) { weightValue in
+                            if !weightValue.allSatisfy({ $0.isNumber || $0 == "." }) {
+                                showAlert = true
+                                alertMessage = "숫자만 입력하세요!"
+                                weight = weightValue.filter { $0.isNumber || $0 == "." }
+                            }
                         }
-                    }
-            }
-            .padding(.horizontal)
-            
-            HStack {
-                Text("나이: ")
-                    .padding()
-                TextField("나이", text: $age)
-                    .keyboardType(.numberPad)
-                    .padding()
-                    .onChange(of: age){ageValue in
-                        if !ageValue.allSatisfy({$0.isNumber || $0 == " "}){
-                            showAlert = true
-                            alertMessage = "키만 입력하세요!"
-                            age = ageValue.filter{$0.isNumber || $0 == " "}
+                }
+                
+                HStack {
+                    Text("나이: ")
+                        .frame(width: 80, alignment: .leading)
+                    TextField("나이", text: $age)
+                        .keyboardType(.numberPad)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .onChange(of: age) { ageValue in
+                            if !ageValue.allSatisfy({ $0.isNumber || $0 == " " }) {
+                                showAlert = true
+                                alertMessage = "숫자만 입력하세요!"
+                                age = ageValue.filter { $0.isNumber || $0 == " " }
+                            }
                         }
-                    }
+                }
             }
             .padding(.horizontal)
         }
         .padding()
-        //showAlert가 true일 때 출력
-        .alert(isPresented: $showAlert){
+        // showAlert가 true일 때 출력
+        .alert(isPresented: $showAlert) {
             Alert(title: Text("잘못된 입력"), message: Text(alertMessage),
                   dismissButton: .default(Text("확인"))
             )
@@ -130,4 +129,3 @@ struct UserInfo: View {
 #Preview {
     UserInfo()
 }
-
